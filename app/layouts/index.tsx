37851065a23dc46e9fff27c264f4a008d473bfa7
Ethan Hathaway <erhathaway@gmail.com>
2020-05-18 21:59:42 -0700

    Feat: add working app

--- app/layouts/index.tsx
+++ app/layouts/index.tsx
@@ -1,21 +1,58 @@
 import React from 'react';
 import styled from 'styled-components';
 import SyntaxHighlighter from 'react-syntax-highlighter';
-import {docco} from 'react-syntax-highlighter/dist/esm/styles/hljs';
-
-const codeString = '(num) => num + 1';
+import {a11yDark} from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
 const Container = styled.div`
-    height: 100%;
-    width: 100%;
+    height: 100vh;
+    width: 100vw;
     background-color: blue;
+    // margin: 1vh 1vw 1vh 1vw;
+    overflow-y: scroll;
+    font-family: 'Space Mono', monospace;
+`;
+
+const Header = styled.div`
+    color: white;
+    padding: 60px;
+    // height: 100px;
+    // width: 50%;
+    // display: flex;
+`;
+
+const Section = styled.div`
+    margin-top: -10px;
+    padding: 40px;
+`;
+
+const SectionTitle = styled.div`
+    color: green;
 `;
 const Layout = (): JSX.Element => {
     return (
         <Container>
-            <SyntaxHighlighter language="javascript" style={docco}>
-                {codeString}
-            </SyntaxHighlighter>
+            <Header>
+                <h1>Beano</h1>
+                <h3>A Pino inspired logger for the browser</h3>
+            </Header>
+
+            <Section>
+                <SectionTitle>
+                    <h3>Install</h3>
+                </SectionTitle>
+                <SyntaxHighlighter language="javascript" style={a11yDark}>
+                    {`import logger from 'beano';`}
+                </SyntaxHighlighter>
+            </Section>
+
+            <Section>
+                <SectionTitle>
+                    <h3>Start logging</h3>
+                </SectionTitle>
+                <SyntaxHighlighter language="javascript" style={a11yDark}>
+                    {`logger.info({groupByMessage: true}, 'look at me!');`}
+                </SyntaxHighlighter>
+            </Section>
         </Container>
     );
 };
